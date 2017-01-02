9f55090d73bf3c4274432d6c8da3361a531ccc4f
Janos Potecki <janospotecki@Janoss-MacBook-Pro.local>
2017-01-02 12:57:01 +0100

    Change: fmlaGen is now fmlaGen' . Add: fmlaGen :: Gen (Fmla) .. allows now to use quickchecks `sample fmlaGen` without specifying a size

--- src/Formula.hs
+++ src/Formula.hs
@@ -73,11 +73,14 @@ negFmla fmla = Neg fmla
 
 -- QuickCheck Stuff
 instance Arbitrary Fmla where
-    arbitrary = scale (\x -> if x < 4 then x else 4) $ sized fmlaGen
+    arbitrary = scale (\x -> if x < 4 then x else 4) $ sized fmlaGen'
 
-fmlaGen :: Int -> Gen (Fmla)
-fmlaGen 0 = propGen
-fmlaGen n = oneof $ fmlaGenerators (n - 1)
+fmlaGen :: Gen (Fmla)
+fmlaGen = fmlaGen' 4
+
+fmlaGen' :: Int -> Gen (Fmla)
+fmlaGen' 0 = propGen
+fmlaGen' n = oneof $ fmlaGenerators (n - 1)
   where
     fmlaGenerators :: Int -> [Gen (Fmla)]
     fmlaGenerators n = [ propGen, (negaGen n) , (conjGen n) , (disjGen n) , (implGen n) ]
@@ -86,13 +89,13 @@ propGen :: Gen (Fmla)
 propGen = elements $ map (\x -> Prop x) allProps
 
 negaGen :: Int -> Gen (Fmla)
-negaGen n = Neg <$> fmlaGen n
+negaGen n = Neg <$> fmlaGen' n
 
 conjGen :: Int ->  Gen (Fmla)
-conjGen n = Conjunction <$> fmlaGen n <*> fmlaGen n
+conjGen n = Conjunction <$> fmlaGen' n <*> fmlaGen' n
 
 disjGen :: Int ->  Gen (Fmla)
-disjGen n = Disjunction <$> fmlaGen n <*> fmlaGen n
+disjGen n = Disjunction <$> fmlaGen' n <*> fmlaGen' n
 
 implGen :: Int ->  Gen (Fmla)
-implGen n = Implication <$> fmlaGen n <*> fmlaGen n
+implGen n = Implication <$> fmlaGen' n <*> fmlaGen' n
